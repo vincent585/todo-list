52e4496862fc6ab8870e98ed22ace744386af368
Vincent Klimjack <vklimjack@gmail.com>
2025-03-24 15:11:52 +0000

    remove notes from task

--- src/models/task.js
+++ src/models/task.js
@@ -1,17 +1,9 @@
 export default class Task {
-    get notes() {
-        return this._notes;
-    }
-
-    set notes(value) {
-        this._notes = value;
-    }
     constructor(title, description, dueDate, priority) {
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
         this.priority = priority;
-        this._notes = "";
         this.checklist = [];
         this.done = false;
     }
